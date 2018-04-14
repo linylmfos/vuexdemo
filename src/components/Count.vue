c855a06ecbff73b75585e6ef1d7a153465e9489e
Amos <>
2018-04-14 16:01:30 +0800

    vuex demo

---
+++ src/components/Count.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2> <hr/>
+        <h3>{{count}}</h3>
+        <p>
+            <button @click="$store.commit('add',10)">+</button>
+            <button @click="reduce">-</button>
+        </p>
+        <p>
+            <button @click="addAction">+</button>
+            <button @click="reduceAction">-</button>
+        </p>
+    </div>    
+</template>
+<script>
+    import store from '@/vuex/store'
+    import { mapState, mapMutations, mapGetters, mapActions } from 'vuex'
+    export default {
+        data () {
+            return {
+                msg: 'Hello Vuex'
+            }
+        },
+        computed: {
+            ...mapState(['count'])
+    
+            // count(){
+            //     this.$store.getters.count
+            // }
+            //...mapGetters(['count'])
+        },
+        // mapState({
+        //     count: state => state.count
+        // }),
+        methods:{
+            ...mapMutations([  
+                'add','reduce'
+            ]),
+            ...mapActions(['addAction','reduceAction'])
+        },
+        store
+    }
+</script>
+<style scoped>
+    button{width: 50px;height:40px;background: red;}
+</style>
+
---
+++ src/vuex/store.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+    count: 3
+}
+const mutations={
+    add(state,n){
+        state.count+=n;
+    },
+    reduce(state) {
+        state.count--;
+    }
+}
+
+const getters = {
+    count: function (state) {
+        return state.count+=10
+    }
+}
+
+const actions ={
+    addAction(context){
+        context.commit('add',10);
+        setTimeout(()=>{context.commit('reduce')},3000);
+        console.log('我比reduce提前执行');
+    },
+    reduceAction({commit}){
+        commit('reduce')
+    }
+}
+
+const moudleA = {
+    state,mutations,getters,actions
+}
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    getters,
+    actions
+})
